79090d3778949ae4389ff31247a7b355777d537e
Alina Sakharova <76452542+sakharlina@users.noreply.github.com>
2021-05-29 19:21:53 +0500

    Create Vector.h

---
+++ Лабораторная работа №18/Lab18.5/Vector.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Object.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Vector
+{
+public:
+	Vector(void);
+	Vector(int);
+public:
+	~Vector(void);
+	void Add(Object*);
+	friend ostream& operator<<(ostream& out, const Vector&);
+private:
+	Object** beg;
+	int size;
+	int cur;
+};
