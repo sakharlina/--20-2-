ee365dced6ed43bff2b8775b45293377e408612d
Alina Sakharova <76452542+sakharlina@users.noreply.github.com>
2021-05-29 19:17:38 +0500

    Create Student.cpp

---
+++ Лабораторная работа №18/Lab18.4/Student.cpp
@@ -0,0 +1,50 @@
+#include "Student.h"
+
+Student::Student(void):Person()
+{
+	year = 0;
+}
+
+Student::~Student(void)
+{
+}
+
+Student::Student(string N, int A, int Y):Person(N, A)
+{
+	year = Y;
+}
+
+Student& Student::operator= (const Student& S)
+{
+	if (&S == this) return *this;
+	name = S.name;
+	age = S.age;
+	year = S.year;
+	return *this;
+}
+
+Student& Student::operator++()
+{
+	int P = year;
+	P++;
+	year = P;
+	return *this;
+}
+
+istream& operator>> (istream& in, Student& s)
+{
+	cout << "Name: "; in >> s.name;
+	cout << "Age: "; in >> s.age;
+	cout << "Year: "; in >> s.year;
+	cout << endl;
+	return in;
+}
+
+ostream& operator<< (ostream& out, const Student& s)
+{
+	out << "\nName: " << s.name;
+	out << "\nAge: " << s.age;
+	out << "\nYear: " << s.year;
+	out << endl;
+	return out;
+}
