6d0be7765e6e0d138a70131be2f11470b49613d3
Alina Sakharova <76452542+sakharlina@users.noreply.github.com>
2021-05-29 19:33:31 +0500

    Create List.h

---
+++ Лабораторная работа №18/Lab18.9/1/List.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+using namespace std;
+const int MAX_SIZE = 30;
+
+class List
+{
+public:
+	int size;
+	int* begin;
+public:
+	List() { size = 0; begin = NULL; }
+	List(int s);
+	List(int s, int*);
+	List(const List& l);
+	~List();
+	const List& operator=(const List&);
+	int operator[](int);
+	List operator+(int);
+	List operator++();
+
+	friend ostream& operator<<(ostream& out, const List& l);
+	friend istream& operator>>(istream& in, List& l);
+};
