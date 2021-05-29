ee7966929c1c119b4830f28eba6fb759e8b90167
Alina Sakharova <76452542+sakharlina@users.noreply.github.com>
2021-05-29 19:22:13 +0500

    Create Vector.cpp

---
+++ Лабораторная работа №18/Lab18.5/Vector.cpp
@@ -0,0 +1,42 @@
+#include "Vector.h"
+
+Vector::Vector(void)
+{
+	beg = 0;
+	size = 0;
+	cur = 0;
+}
+
+Vector::~Vector(void)
+{
+	if (beg != 0) delete[] beg;
+	beg = 0;
+}
+
+Vector::Vector(int n)
+{
+	beg = new Object * [n];
+	cur = 0;
+	size = n;
+}
+
+void Vector::Add(Object* t)
+{
+	if (cur < size)
+	{
+		beg[cur] = t;
+		cur++;
+	}
+}
+
+ostream& operator<<(ostream& out, const Vector& v)
+{
+	if (v.size == 0) out << "Empty" << endl;
+	Object** t = v.beg;
+	for (int i = 0; i < v.cur; i++)
+	{
+		(*t)->Show();
+		t++;
+	}
+	return out;
+}
